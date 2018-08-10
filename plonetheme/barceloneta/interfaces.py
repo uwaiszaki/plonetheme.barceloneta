5de2eddc0a48eced4809fa6dc7b292732118430b
Jens W. Klein <jk@kleinundpartner.at>
2018-08-10 11:35:10 +0200

    look at both places for default layer

--- plonetheme/barceloneta/interfaces.py
+++ plonetheme/barceloneta/interfaces.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
-from plone.theme.interfaces import IDefaultPloneLayer
+try:
+    # plone.theme was moved to Products.CMFPlone in Plone 5.2
+    #
+    # can be removed ass soon as this package is not used for Plone < 5.2
+    # any more
+    from Products.CMFPlone.interfaces import IDefaultPloneLayer
+except ImportError:
+    from plone.theme.interfaces import IDefaultPloneLayer
 
 
 class IBarcelonetaLayer(IDefaultPloneLayer):
