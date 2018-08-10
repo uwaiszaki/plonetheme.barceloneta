18bf0fe71e89b693b7aea311c6e81d15fe18682e
Jens W. Klein <jk@kleinundpartner.at>
2018-08-10 11:34:41 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 from setuptools import setup, find_packages
 
@@ -5,49 +6,45 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+
 version = '1.8.3.dev0'
 
-long_description = (
-    read('README.rst') + '\n' +
-    read('CHANGES.rst') + '\n'
-    )
+long_description = read('README.rst') + '\n' + read('CHANGES.rst') + '\n'
 
-setup(name='plonetheme.barceloneta',
-      version=version,
-      description="The default theme for Plone 5.",
-      long_description=long_description,
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Plone :: 5.0",
-          "Framework :: Plone :: 5.1",
-          "Framework :: Plone :: Theme",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          ],
-      keywords='web zope plone theme',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='https://pypi.python.org/pypi/plonetheme.barceloneta',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plonetheme'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.app.theming',
-          'plone.batching >1.0.999'
-      ],
-      extras_require={
-          'test': [
-          ]
-      },
-      entry_points="""
+setup(
+    name='plonetheme.barceloneta',
+    version=version,
+    description="The default theme for Plone 5.",
+    long_description=long_description,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 5.0",
+        "Framework :: Plone :: 5.1",
+        "Framework :: Plone :: Theme",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    keywords='web zope plone theme',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='https://pypi.python.org/pypi/plonetheme.barceloneta',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plonetheme'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'plone.app.theming',
+        'plone.batching >1.0.999',
+    ],
+    extras_require={'test': []},
+    entry_points="""
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      )
+)
